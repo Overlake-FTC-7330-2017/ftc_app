fa7d9273f220b3cac87ccfa67f0320dfddb2ab4d
unholydeath785 <ivansonofcoul@gmail.com>
2018-02-10 10:11:18 -0800

    squash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/controller/Controller.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/controller/Controller.java
@@ -332,5 +332,7 @@ public class Controller {
         rightBumper.testAndHandle();
         rightStickButton.testAndHandle();
         rightTrigger.testAndHandle();
+        dPadDownShifted.testAndHandle();
+        
     }
 }
